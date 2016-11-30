83af9b3bbb14431e418819e09eb189290147b122
Alberto Pires. <alberto.pires.on@gmail.com>
2016-11-30 18:18:17 -0200

    Sending something back.

--- src/main/java/io/github/albertopires/study/DiscardServerHandler.java
+++ src/main/java/io/github/albertopires/study/DiscardServerHandler.java
@@ -3,13 +3,31 @@ package io.github.albertopires.study;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+import java.util.Date;
 
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) {
-		// Discard the received data silently.
-		((ByteBuf) msg).release();
+		ByteBuf in = (ByteBuf) msg;
+
+		try {
+			while (in.isReadable()) {
+				System.out.print((char) in.readByte());
+				System.out.flush();
+			}
+		} finally {
+			// ((ByteBuf) msg).release();
+			System.err.println("Releasing msg");
+			Date d = new Date();
+			String str = d.toString();
+			ByteBuf response = ctx.alloc().buffer(str.length());
+			response.writeBytes(str.getBytes());
+			ctx.writeAndFlush(response);
+			ReferenceCountUtil.release(msg);
+		}
 	}
 
 	@Override
@@ -18,5 +36,5 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 		cause.printStackTrace();
 		ctx.close();
 	}
-
+	
 }
